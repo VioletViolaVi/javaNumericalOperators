a32f59962cb9a10fb33424830b6f454806d49025
VioletViola <vivian.v18723@gmail.com>
2021-08-15 16:53:53 +0100

    Practice using numerical operators in java.

---
+++ src/NumericalOperators.java
@@ -0,0 +1,9 @@
+public class NumericalOperators {
+    public static void main(String[] args) {
+        System.out.println(10 % 7);
+        System.out.println(7 * 7);
+        System.out.println(7 / 7);
+        System.out.println(7 - 7);
+        System.out.println(7 + 7);
+    }
+}
